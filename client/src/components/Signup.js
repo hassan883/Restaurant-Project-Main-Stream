a218dd0917a8ae70774b4e0c1814626fa63cb246
Hassan Javed <hassanjaved917127@gmail.com>
2022-10-24 20:43:01 +0500

    signup form added 6tuts added

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import './Signup.css';
+const Signup = () => {
 
-const Signup = ()=> {
     return (
-        <p>Inside Signup Components</p>
-        );
+        <>
+            <div className="signup-container">
+                <div className="row px-3 vh-100">
+                    <div className="col-md-5 mx-auto align-self-center">
+                        <form className="row g-3">
+                            <div className="col-md-6">
+                                <label for="inputUsername" className="form-label">UserName</label>
+                                <input type="text" className="form-control" id="inputUsername" />
+                            </div>
+                            <div className="col-md-6">
+                                <label for="inputEmail" className="form-label">Email</label>
+                                <input type="email" className="form-control" id="inputEmail" />
+                            </div>
+
+                            <div className="col-12">
+                                <label for="inputPassword" className="form-label">Password</label>
+                                <input type="password" className="form-control" id="inputPassword" placeholder="Enter you password" />
+                            </div>
+                            <div className="col-12">
+                                <label for="confirmPassword" className="form-label">Confirm Password</label>
+                                <input type="password" className="form-control" id="confirmPassword" placeholder="Enter you confirm password" />
+                            </div>
+
+
+                            <div className="col-12">
+                                <button type="submit" className="btn btn-primary">Sign up</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default Signup;
