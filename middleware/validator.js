c976eadc5169c77fd5671e5b895247aaa680432a
Hassan Javed <hassanjaved917127@gmail.com>
2022-11-03 11:53:09 +0500

    middlewares added tuts12

---
+++ middleware/validator.js
@@ -0,0 +1,34 @@
+const {check, validationResult} = require('express-validator');
+
+
+
+exports.signupValidator = [
+    check('username')
+        .not().isEmpty()
+        .trim()
+        .withMessage("All Fields are required"),
+    check('email')
+        .isEmail()
+        .normalizeEmail()
+        .withMessage("Invalid Email"),
+    check('password')
+        .isLength({min: 6})
+        .withMessage("Password must be at least 6 character long")
+
+]
+
+exports.validatorResult = (req, res, next) =>{
+    const result = validationResult(req);
+    const hasError = !result.isEmpty();
+    if(hasError){
+        const firstError = result.array()[0].msg;
+        return res.status(400).json({
+            errorMessage: firstError,
+        })
+
+        // console.log("HasError : ", hasError);
+        // console.log("Result : ", result);
+    }
+
+    next();
+}
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const {signupValidator, validatorResult} = require('../middleware/validator');
 
-router.post('/signup',(req, res)=>{
-    console.log("Inside Signup controller");
-});
+router.post('/signup',signupValidator, validatorResult);
 
 
 module.exports = router;
